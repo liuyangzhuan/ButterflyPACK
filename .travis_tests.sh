778135a5ed323e7c540452417e762a6ac4a3b7cf
liuyangz <liuyangzhuan@gmail.com>
2018-10-15 18:11:09 -0700

    minor change

--- .travis_tests.sh
+++ .travis_tests.sh
@@ -11,6 +11,6 @@ export EXAMPLE_FOLDER=$TRAVIS_BUILD_DIR/build/EXAMPLE
 
 case "${TEST_NUMBER}" in
 1) mpirun "-n" "2" "$EXAMPLE_FOLDER/fkerreg" "$DATA_FOLDER/SUSY/susy_10Kn" "8" "10000" "1000" "0.1" "1.0" "2" "2";;
-2) mpirun "-n" "2" "$EXAMPLE_FOLDER/fem2d" ;;
+2) mpirun "-n" "2" "$EXAMPLE_FOLDER/fem2d" "1";;
 *) printf "${RED} ###GC: Unknown test\n" ;;
 esac
