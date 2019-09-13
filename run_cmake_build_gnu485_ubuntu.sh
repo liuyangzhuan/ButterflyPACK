4335a8f1dc3dc6c31ffabd4df0e4377cff591cc3
liuyangz <liuyangzhuan@gmail.com>
2019-09-13 12:51:54 -0700

    fixed buid issue with gnu485

--- run_cmake_build_gnu485_ubuntu.sh
+++ run_cmake_build_gnu485_ubuntu.sh
@@ -21,17 +21,17 @@
     -DCMAKE_CXX_FLAGS="-fopenmp" \
     -Denable_blaslib=OFF \
     -DBUILD_SHARED_LIBS=OFF \
-    -DCMAKE_C_COMPILER=mpicc \
-    -DCMAKE_CXX_COMPILER=mpic++ \
-    -DCMAKE_Fortran_COMPILER=mpif90 \
+    -DCMAKE_C_COMPILER=/usr/bin/mpicc \
+    -DCMAKE_CXX_COMPILER=/usr/bin/mpic++ \
+    -DCMAKE_Fortran_COMPILER=/usr/bin/mpif90 \
     -DCMAKE_INSTALL_PREFIX=. \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
     -DCMAKE_Fortran_FLAGS="-fopenmp" \
     -DTPL_BLAS_LIBRARIES="$BLAS_LIB" \
     -DTPL_LAPACK_LIBRARIES="$LAPACK_LIB" \
-    -DTPL_SCALAPACK_LIBRARIES="$SCALAPACK_LIB" \
-    -DTPL_ARPACK_LIBRARIES="$ARPACK_LIB"
+    -DTPL_SCALAPACK_LIBRARIES="$SCALAPACK_LIB"
+ #   -DTPL_ARPACK_LIBRARIES="$ARPACK_LIB"
  make
  make install
 
