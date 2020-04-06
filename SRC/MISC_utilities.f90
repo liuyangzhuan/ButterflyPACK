b6a15953fd2c1a725e41096672483cbca0b04366
liuyangzhuan <liuyangzhuan@gmail.com>
2020-04-05 23:51:37 -0700

    further improved BPACK_al2all*

--- SRC/MISC_utilities.f90
+++ SRC/MISC_utilities.f90
@@ -3951,15 +3951,14 @@ contains
       IOwnPgrp = ptree%MyID >= ptree%pgrp(pgno)%head .and. ptree%MyID <= ptree%pgrp(pgno)%tail
    end function IOwnPgrp
 
-! convert global index to local index in block-cyclic distribution
-   subroutine g2l(i, n, np, nb, p, il)
+   pure subroutine g2l(i, n, np, nb, p, il)
       implicit none
-      integer :: i    ! global array index, input
-      integer :: n    ! global array dimension, input
-      integer :: np   ! processor array dimension, input
-      integer :: nb   ! block size, input
-      integer :: p    ! processor array index, output
-      integer :: il   ! local array index, output
+      integer,intent(in) :: i    ! global array index, input
+      integer,intent(in) :: n    ! global array dimension, input
+      integer,intent(in) :: np   ! processor array dimension, input
+      integer,intent(in) :: nb   ! block size, input
+      integer,intent(out) :: p    ! processor array index, output
+      integer,intent(out) :: il   ! local array index, output
       integer :: im1
       im1 = i - 1
       p = mod((im1/nb), np)
@@ -3968,14 +3967,14 @@ contains
    end subroutine g2l
 
 ! convert local index to global index in block-cyclic distribution
-   subroutine l2g(il, p, n, np, nb, i)
+   pure subroutine l2g(il, p, n, np, nb, i)
       implicit none
-      integer :: il   ! local array index, input
-      integer :: p    ! processor array index, input
-      integer :: n    ! global array dimension, input
-      integer :: np   ! processor array dimension, input
-      integer :: nb   ! block size, input
-      integer :: i    ! global array index, output
+      integer,intent(in) :: il   ! local array index, input
+      integer,intent(in) :: p    ! processor array index, input
+      integer,intent(in) :: n    ! global array dimension, input
+      integer,intent(in) :: np   ! processor array dimension, input
+      integer,intent(in) :: nb   ! block size, input
+      integer,intent(out) :: i    ! global array index, output
       integer :: ilm1
       ilm1 = il - 1
       i = (((ilm1/nb)*np) + p)*nb + mod(ilm1, nb) + 1
