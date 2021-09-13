683f4d0cbeb833b2bbe1af0442abbb832959fe49
liuyangzhuan <liuyangzhuan@gmail.com>
2021-09-13 11:09:20 -0700

    added a single-precision ie3d driver, results need to be validated

--- SRC/BPACK_defs.f90
+++ SRC/BPACK_defs.f90
@@ -32,7 +32,7 @@ module BPACK_DEFS
     INCLUDE 'mpif.h'
 #endif
     integer, parameter:: BPACK_MAJOR_VERSION = 2
-    integer, parameter:: BPACK_MINOR_VERSION = 0
+    integer, parameter:: BPACK_MINOR_VERSION = 1
     integer, parameter:: BPACK_PATCH_VERSION = 0
 
     !**** common parameters
